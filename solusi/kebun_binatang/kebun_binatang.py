6adcf60be999396ce78bf656fe130e6930db9f5c
pratamaputra-pemula <wijangpratamaputra@gmail.com>
2026-06-01 20:32:40 +0700

    Update kebun_binatang.py

--- solusi/kebun_binatang/kebun_binatang.py
+++ solusi/kebun_binatang/kebun_binatang.py
@@ -3,6 +3,29 @@
 # Dikerjakan oleh: Wijang pratama (OCP) & Abid (DIP)
 
 from kandang.kandang import KandangBase
+from antarmuka.bisa_terbang import BisaTerbang
+from antarmuka.bisa_berenang import BisaBerenang
+from antarmuka.bisa_berlari import BisaBerlari
+
+class Perawatan:
+    """Class khusus merawat hewan (SRP)
+       Tidak perlu diubah jika ada hewan baru (OCP)"""
+    def __init__(self, kandang: KandangBase):  # DIP - inject lewat parameter
+        self.kandang = kandang
+
+    def rawat_semua_hewan(self):
+        print("\n--- Merawat Semua Hewan ---")
+        for hewan in self.kandang.get_semua_hewan():
+            hewan.makan()
+            if isinstance(hewan, BisaTerbang):   # OCP - tidak paksa terbang()
+                hewan.terbang()
+            if isinstance(hewan, BisaBerenang):  # OCP - tidak paksa berenang()
+                hewan.berenang()
+            if isinstance(hewan, BisaBerlari):   # OCP - tidak paksa berlari()
+                hewan.berlari()
+
+
+from kandang.kandang import KandangBase
 from layanan.perawatan import Perawatan
 from layanan.pemberian_makan import PemberianMakan
 
